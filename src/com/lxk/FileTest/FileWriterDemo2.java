c8324806e913f39d67e32fd379506ee36e0de78b
cmshome <cmshome@163.com>
2016-11-20 12:57:04 +0800

    文件流：处理IO异常，正确使用try catch finally

--- src/com/lxk/FileTest/FileWriterDemo2.java
+++ src/com/lxk/FileTest/FileWriterDemo2.java
@@ -1,36 +1,25 @@
-package com.lxk.FileTest;/*
-IO异常的处理方式。
-*/
+package com.lxk.FileTest;
+
 import java.io.*;
 
-class  FileWriterDemo2
-{
-	public static void main(String[] args) 
-	{
-		FileWriter fw = null;
-		try
-		{
-			fw = new FileWriter("demo.txt");
-			fw.write("abcdefg");
-
-		}
-		catch (IOException e)
-		{
-			System.out.println("catch:"+e.toString());
-		}
-		finally
-		{
-			try
-			{
-				if(fw!=null)
-					fw.close();				
-			}
-			catch (IOException e)
-			{
-				System.out.println(e.toString());
-			}
-			
-		}		
-
-	}
+/**
+ * IO异常的处理方式。
+ */
+class FileWriterDemo2 {
+    public static void main(String[] args) {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter("demo.txt");
+            fw.write("abcdefg");
+        } catch (IOException e) {
+            System.out.println("catch:" + e.toString());
+        } finally {
+            try {
+                if (fw != null)
+                    fw.close();
+            } catch (IOException e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
 }
