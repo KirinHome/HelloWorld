0d2012bfc0722ae3a0a467e9bd2bf7a16ffabda1
cmshome <cmshome@163.com>
2016-12-05 09:44:33 +0800

    个人测试

--- src/com/lxk/test/Lxk.java
+++ src/com/lxk/test/Lxk.java
@@ -1,10 +1,18 @@
 package com.lxk.test;
 
+import java.util.List;
+
 /**
  * Created by lxk on 2016/11/8
  */
 public class Lxk {
     public static void main(String[] args) {
         System.out.println(1/10);
+        String s = "bbbaaa";
+        System.out.println(s.replace("*",".*"));
+        List<String> ss = null;
+        String qw = -1 > 0 ? "true" :"false";
+
+        System.out.println(qw);
     }
 }
