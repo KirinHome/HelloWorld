95f535fa70b545f07857da9e508755c67c97c386
cmshome <cmshome@163.com>
2017-02-07 14:46:54 +0800

    求答案 ？ 一筐鸡蛋： 1个1个拿，正好拿完。 2个2个拿，还剩1个。 3个3个拿，正好拿完。 4个4个拿，还剩1个。 5个5个拿，还差1个。 6个6个拿，还剩3个。 7个7个拿，还剩4个。 8个8个拿，还剩1个。 9个9个拿，正好拿完。
    问筐里最少有多少鸡蛋？

--- src/com/lxk/test/ManLxk.java
+++ src/com/lxk/test/ManLxk.java
@@ -24,12 +24,11 @@ public class ManLxk extends Person {
             if (i % 2 == 1
                     && i % 3 == 0
                     && i % 4 == 1
-                    && i % 5 == 1
+                    && i % 5 == 4
                     && i % 6 == 3
                     && i % 7 == 4
                     && i % 8 == 1
-                    && i % 9 == 0
-                    ) {
+                    && i % 9 == 0) {
                 System.out.println(i);
                 return;
             }
