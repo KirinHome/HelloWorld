52bd3ef6a9d8cef0ff11627bfeb61a6e72dd76f3
cmshome <cmshome@163.com>
2017-02-15 16:18:30 +0800

    ArrayList 测试

--- src/com/lxk/collectionTest/ArrayListTest.java
+++ src/com/lxk/collectionTest/ArrayListTest.java
@@ -9,13 +9,16 @@ import java.util.List;
 public class ArrayListTest {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(2);
-        System.out.println(list.size());
+        System.out.println("list大小为：" + list.size());
         list.add("12");
-        System.out.println(list.size());
+        System.out.println("list大小为：" + list.size());
         list.set(1,"sss");
+        list.add(1,"放在下标为1的位置");
         list.add("22");
-        System.out.println(list.size());
-
+        System.out.println("list大小为：" + list.size());
+        System.out.println(list.toString());
+        list.add(1,"放在下标为1的位置");
+        System.out.println("list大小为：" + list.size());
         System.out.println(list.toString());
     }
 }
