213f63d9b3b556b1f7e3314d7ec740d4d563fa52
cmshome <cmshome@163.com>
2017-02-15 18:22:55 +0800

    代码格式化

--- src/com/lxk/collectionTest/ArrayListTest.java
+++ src/com/lxk/collectionTest/ArrayListTest.java
@@ -12,12 +12,12 @@ public class ArrayListTest {
         System.out.println("list大小为：" + list.size());
         list.add("12");
         System.out.println("list大小为：" + list.size());
-        list.set(1,"sss");
-        list.add(1,"放在下标为1的位置");
+        list.set(1, "sss");
+        list.add(1, "放在下标为1的位置");
         list.add("22");
         System.out.println("list大小为：" + list.size());
         System.out.println(list.toString());
-        list.add(1,"放在下标为1的位置");
+        list.add(1, "放在下标为1的位置");
         System.out.println("list大小为：" + list.size());
         System.out.println(list.toString());
     }
