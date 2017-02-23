21cb18c77744dd6abfc5af5f80826bf4b9fab7a7
cmshome <cmshome@163.com>
2017-02-23 15:27:57 +0800

    代码块的测试

--- src/com/lxk/test/StringTest.java
+++ src/com/lxk/test/StringTest.java
@@ -14,9 +14,9 @@ public class StringTest {
         if (false) {
             testValueAndAddressTransmit();
             testStringBufferAndStringBuilder();
-            testStringPool();
-        } else {
             testStringNewLine();
+        } else {
+            testStringPool();
         }
     }
 
@@ -90,17 +90,17 @@ public class StringTest {
             这样做实际上是一种优化，避免了创建多余的字符串对象，也没有发生字符串拼接问题
          */
         String d = "ab" + "c";//字面量形式
-        System.out.println("a == b " + (a == b));
-        System.out.println("a == c " + (a == c));
-        System.out.println("a == d " + (a == d));
-        System.out.println("b == c " + (b == c));
-        System.out.println("b == d " + (b == d));
-        System.out.println("c == d " + (c == d));
+        System.out.println("a == b " + (a == b));//true
+        System.out.println("a == c " + (a == c));//false
+        System.out.println("a == d " + (a == d));//true
+        System.out.println("b == c " + (b == c));//false
+        System.out.println("b == d " + (b == d));//true
+        System.out.println("c == d " + (c == d));//false
         System.out.println("-----------------");
-        System.out.println("abc" == ("ab" + "c"));
+        System.out.println("abc" == ("ab" + "c"));//true
         System.out.println("-----------------");
         String e = c.intern();//将new出来的字符串对象加入字符串常量池
-        System.out.println(a == e);
+        System.out.println(a == e);//true
         /*
             Java中字符串对象创建有两种形式。
             一种为字面量形式，如String str = "droid";，
