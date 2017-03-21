7ee69f923e93dca7b8b6d66595f2f5eec104c0b1
cmshome <cmshome@163.com>
2017-03-21 16:39:40 +0800

    Calendar.get(int )方法测试

--- src/com/lxk/test/TimeTest.java
+++ src/com/lxk/test/TimeTest.java
@@ -3,6 +3,7 @@ package com.lxk.test;
 import com.lxk.model.TimeFormatModel;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -15,12 +16,29 @@ public class TimeTest {
         //Date date = new Date();
         //Date aw = Calendar.getInstance().getTime();//获得时间的另一种方式，测试效果一样
         //testTime(date);
-        TimeFormatModel t = new TimeFormatModel();
-        t.setDate(new Date());
-        long a = System.currentTimeMillis();
-        System.out.println(t.toString());
-        System.out.println("\r<br> 执行耗时 : " + (System.currentTimeMillis() - a) / 1000f + " 秒 ");
+        //long a = System.currentTimeMillis();
+        //System.out.println(t.toString());
+        //System.out.println("\r<br> 执行耗时 : " + (System.currentTimeMillis() - a) / 1000f + " 秒 ");
+        testCalendar();
     }
+
+    /**
+     * 查看 Calendar 获得年月日时分秒
+     */
+    private static void testCalendar() {
+        Calendar cal =Calendar.getInstance();
+        TimeFormatModel t = new TimeFormatModel();
+        t.setDate(cal.getTime());
+        System.out.println(t.toString());
+        System.out.println(cal.get(Calendar.YEAR));
+        System.out.println(cal.get(Calendar.MONTH));
+        System.out.println(cal.get(Calendar.DAY_OF_MONTH) + 1);
+        System.out.println(cal.get(Calendar.HOUR_OF_DAY));
+        System.out.println(cal.get(Calendar.MINUTE));
+        System.out.println(cal.get(Calendar.SECOND));
+
+    }
+
 
     /**
      * 将Date类型的时间转成 2016-10-25 18:11:55 此格式
