9cf60c55959cd11aa8a2f6d1ce078160aa745743
cmshome <cmshome@163.com>
2017-04-20 19:34:16 +0800

    测试Java类的成员和初始化块（代码块）初始化顺序

---
+++ src/com/lxk/ClassInitTest/Child.java
@@ -0,0 +1,34 @@
+package com.lxk.ClassInitTest;
+
+/**
+ * 子类
+ * <p>
+ * Created by lxk on 2017/4/20
+ */
+public class Child extends Parent {
+
+    private String stringInChild = initStringInChild();
+    private static String staticStringInChild = initStaticStringInChild();
+
+    {
+        System.out.println("子类：普通代码块");
+    }
+
+    static {
+        System.out.println("子类：静态代码块");
+    }
+
+    public Child() {
+        System.out.println("子类：构造方法");
+    }
+
+    private static String initStaticStringInChild() {
+        System.out.println("子类：静态方法，被静态成员变量赋值调用。");
+        return "initStaticStringInChild";
+    }
+
+    private String initStringInChild() {
+        System.out.println("子类：普通成员方法，被普通成员变量赋值调用。");
+        return "initStringInChild";
+    }
+}
---
+++ src/com/lxk/ClassInitTest/ClassInitTest.java
@@ -0,0 +1,14 @@
+package com.lxk.ClassInitTest;
+
+/**
+ * 测试Java类的成员和初始化块（代码块）初始化顺序
+ * <p>
+ * Created by lxk on 2017/4/20
+ */
+public class ClassInitTest {
+    public static void main(String[] args) {
+        System.out.println("测试代码开始");
+        new Child();
+        System.out.println("测试代码结束");
+    }
+}
