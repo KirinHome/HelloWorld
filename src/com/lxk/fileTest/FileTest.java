b6087dd2c9a382581e0b6517d49a0ecae0fcee8d
cmshome <cmshome@163.com>
2017-05-16 19:30:22 +0800

    文件目录不存在则创建的测试

---
+++ src/com/lxk/fileTest/FileTest.java
@@ -0,0 +1,52 @@
+package com.lxk.fileTest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Properties;
+
+/**
+ * 文件测试
+ * <p>
+ * Created by lxk on 2017/5/16
+ */
+public class FileTest {
+    public static void main(String[] args) {
+        testFileIsExists();
+    }
+
+    /**
+     * 一个目录要是不存在，则创建目录，然后写文件。
+     */
+    private static void testFileIsExists() {
+        String path = "D:/lxk/conf/es-source.properties";
+        File file = new File(path);
+        System.out.println(file.getParentFile());
+        if (!file.getParentFile().exists()) {
+            boolean result = file.getParentFile().mkdirs();
+            if (!result) {
+                System.out.println("创建失败");
+            }
+        }
+        Properties properties = new Properties();
+        properties.setProperty("sss", "ssa");
+        properties.setProperty("ssasds", "ssaas");
+        OutputStreamWriter outputStreamWriter = null;
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(path, false);//true表示追加打开,false每次都是清空再重写
+            outputStreamWriter = new OutputStreamWriter(fileOutputStream, "utf-8");
+            properties.store(outputStreamWriter, "");
+        } catch (Exception e) {
+            System.out.println("writeOrderedPropertiesFile IOException:" + e.getMessage());
+        } finally {
+            if (outputStreamWriter != null) {
+                try {
+                    outputStreamWriter.close();
+                } catch (IOException e) {
+                    System.out.println("writeOrderedPropertiesFile close IOException:" + e.getMessage());
+                }
+            }
+        }
+    }
+}
