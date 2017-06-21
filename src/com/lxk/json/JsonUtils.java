05c3215275adebcca467ae38caa89198eeb55804
cmshome <cmshome@163.com>
2017-06-21 15:48:33 +0800

    测试push

--- src/com/lxk/json/JsonUtils.java
+++ src/com/lxk/json/JsonUtils.java
@@ -33,7 +33,7 @@ public final class JsonUtils {
      */
     public static <T> T parseJsonToObj(String json, Class<T> c) {
         try {
-            //两个都是可行的，起码我测试的时候是没问题的。
+            //两个都是可行的，起码我测试的时候是没问题 的。
             //JSONObject jsonObject = JSONObject.parseObject(json);
             //JSONObject jsonObject = JSON.parseObject(json, Feature.OrderedField);
             JSONObject jsonObject = JSON.parseObject(json);
