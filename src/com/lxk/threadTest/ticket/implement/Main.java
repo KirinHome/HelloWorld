250faf20e9eb4e5c43f5f485e13ec1e32869f505
cmshome <cmshome@163.com>
2017-06-26 17:12:14 +0800

    多线程：静态同步函数中使用的锁是类名.class，不是this。

--- src/com/lxk/threadTest/ticket/implement/Main.java
+++ src/com/lxk/threadTest/ticket/implement/Main.java
@@ -7,6 +7,11 @@ package com.lxk.threadTest.ticket.implement;
  */
 public class Main {
     public static void main(String[] args) {
+        //test();
+        testStatic();
+    }
+
+    private static void test() {
         Ticket t = new Ticket();
 
         Thread t1 = new Thread(t);
@@ -17,7 +22,10 @@ public class Main {
         //这时候，没有添加下面的sleep的时候，代码一运行，所有执行结果全走的是同步函数，
         //因为线程1启动完之后，瞬间，主线程已经把flag置成false啦，所以，都走的是false结果。
         //所以，要在这1线程启动完之后，主线程休息一下才能看到2个线程分别的效果。
-        try{Thread.sleep(10);}catch(Exception ignore){}
+        try {
+            Thread.sleep(10);
+        } catch (Exception ignore) {
+        }
         t.flag = false;
         t2.start();
 
@@ -33,5 +41,32 @@ public class Main {
         //Thread t4 = new Thread(t);
         //t3.start();
         //t4.start();
+
     }
+
+    private static void testStatic() {
+        TicketStatic t = new TicketStatic();
+        Thread t1 = new Thread(t);
+        Thread t2 = new Thread(t);
+        t1.start();
+        //这时候，没有添加下面的sleep的时候，代码一运行，所有执行结果全走的是同步函数，
+        //因为线程1启动完之后，瞬间，主线程已经把flag置成false啦，所以，都走的是false结果。
+        //所以，要在这1线程启动完之后，主线程休息一下才能看到2个线程分别的效果。
+        try {
+            Thread.sleep(10);
+        } catch (Exception ignore) {
+        }
+        t.flag = false;
+        t2.start();
+
+        //运行结果：打印出0的错票。不安全。（这个时候，同步代码块使用的锁，是自己this）
+        //静态方法使用的锁和同步代码块使用的锁不一样。静态同步函数使用的锁是类.class
+        //对错误代码进行分析如下：
+        //两个前提。1，两个或以上的线程；2，用的是否是同一个锁。
+        // 后面修改同步函数中的同步对象由this变成.class，然后就安全啦。
+        //
+        //这个修改完之后，就可以看到，没有输出0啦，而且2个线程，确实都执行了不同的同步实现。一个同步代码块，一个同步函数。
+
+    }
+
 }
---
+++ src/com/lxk/threadTest/ticket/implement/TicketStatic.java
@@ -0,0 +1,44 @@
+package com.lxk.threadTest.ticket.implement;
+
+/**
+ * 测试：静态同步函数和非静态的差别
+ * <p>
+ * Created by lxk on 2017/6/25
+ */
+public class TicketStatic implements Runnable {
+    private static int tick = 100;
+    boolean flag = true;
+
+    public void run() {
+        if (flag) {
+            while (true) {
+                //synchronized (this) {//静态同步函数使用的是类对象。
+                //内存中没有本类对象，但是一定有该类对应的字节码文件对象。
+                //类名.class  该对象的类型是Class
+                synchronized (TicketStatic.class) {
+                    if (tick > 0) {
+                        try {
+                            Thread.sleep(10);
+                        } catch (Exception ignore) {
+                        }
+                        System.out.println(Thread.currentThread().getName() + "....sale...代码块 : " + tick--);
+                    }
+                }
+            }
+        } else {
+            while (true) {
+                show();
+            }
+        }
+    }
+
+    private static synchronized void show() {//使用的锁就是this
+        if (tick > 0) {
+            try {
+                Thread.sleep(10);
+            } catch (Exception ignore) {
+            }
+            System.out.println(Thread.currentThread().getName() + "....sale...函数 : " + tick--);
+        }
+    }
+}
