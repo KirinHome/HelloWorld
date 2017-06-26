20620d21d4eef5ffb2f128232f182ece00ba820e
cmshome <cmshome@163.com>
2017-06-26 14:27:45 +0800

    多线程：修改加锁机制--同步代码块<-->同步函数

--- src/com/lxk/threadTest/ticket/implement/Ticket.java
+++ src/com/lxk/threadTest/ticket/implement/Ticket.java
@@ -4,16 +4,21 @@ package com.lxk.threadTest.ticket.implement;
  * Created by lxk on 2017/6/25
  */
 public class Ticket implements Runnable {
-    private int tick = 1000;
+    private int tick = 100;
 
     public void run() {
         while (true) {
-            synchronized (this) {
-                if (tick > 0) {
-                    try{Thread.sleep(10);}catch(Exception ignore){}
-                    System.out.println(Thread.currentThread().getName() + "....sale : " + tick--);
-                }
+            show();
+        }
+    }
+
+    private synchronized void show() {//使用的锁就是this
+        if (tick > 0) {
+            try {
+                Thread.sleep(10);
+            } catch (Exception ignore) {
             }
+            System.out.println(Thread.currentThread().getName() + "....sale : " + tick--);
         }
     }
 }
