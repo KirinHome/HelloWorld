ba29f95a1b0cb563956e5377c30579076f9808f1
cmshome <cmshome@163.com>
2017-06-27 18:10:31 +0800

    多线程：刚刚知道实现线程的第三种方式：实现Callable接口

--- src/com/lxk/threadTest/wayToThread/Main.java
+++ src/com/lxk/threadTest/wayToThread/Main.java
@@ -7,20 +7,22 @@ package com.lxk.threadTest.wayToThread;
  */
 public class Main {
     public static void main(String[] args) {
-        //newThreadWay1();
-        //newThreadWay1_();
-        //newThreadWay1__();
-        newThreadWay2();
+        newThreadWay1();//实现Runnable接口
+        //newThreadWay2();//继承Thread类
+        //newThreadWay3();//实现Callable接口
+        //newThreadWay1_();//也就是个简单的匿名对象，直接实现Runnable接口，省去了新建个类的步骤
+        //newThreadWay1__();//一样也是实现Runnable接口，省去了新建个类的步骤，不过不是匿名对象而已
     }
 
     /**
-     * 创建线程方式1实现(和上面稍微有点差别)
+     * 创建线程方式1实现
      * 实现Runnable接口，重写run方法
+     * 实现接口的优点：可以方便扩展
      */
     private static void newThreadWay1() {
-        //一个类实现Runnable接口，可以方便扩展
         ThreadWay1 threadWay1 = new ThreadWay1();
         new Thread(threadWay1).start();
+        //new Thread(new ThreadWay1()).start();//等于上面的2行代码
     }
 
 
@@ -31,12 +33,27 @@ public class Main {
     private static void newThreadWay2() {
         ThreadWay2 threadWay2 = new ThreadWay2();
         threadWay2.start();
-        //new ThreadWay2().start();等于上面的2行代码
+        //new ThreadWay2().start();//等于上面的2行代码
     }
 
     /**
-     * 创建线程方式1实现(匿名对象：new个线程对象直接开启)
+     * 创建线程方式3实现
+     * 实现Callable接口，重写call方法
+     */
+    private static void newThreadWay3() {
+        int resultFromThread;
+        try {
+            resultFromThread = new ThreadWay3().call();
+            System.out.println(resultFromThread);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建线程方式1实现
      * 实现Runnable接口，重写run方法
+     * (匿名对象：new个线程对象直接开启)
      */
     private static void newThreadWay1_() {
         new Thread(new Runnable() {
@@ -56,8 +73,9 @@ public class Main {
     }
 
     /**
-     * 创建线程方式1实现(实例化一个线程对象，然后调用start方法开启线程)
+     * 创建线程方式1实现
      * 实现Runnable接口，重写run方法
+     * (实例化一个线程对象，然后调用start方法开启线程)
      */
     private static void newThreadWay1__() {
         Thread thread = new Thread(new Runnable() {
---
+++ src/com/lxk/threadTest/wayToThread/ThreadWay3.java
@@ -0,0 +1,30 @@
+package com.lxk.threadTest.wayToThread;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Callable和Runnable有几点不同：
+ * （1）Callable规定的方法是call()，而Runnable规定的方法是run().
+ * （2）Callable的任务执行后可返回值，而Runnable的任务是不能返回值的。
+ * （3）call()方法可抛出异常，而run()方法是不能抛出异常的。
+ * （4）运行Callable任务可拿到一个Future对象，
+ * <p>
+ * Created by lxk on 2017/6/27
+ */
+public class ThreadWay3 implements Callable {
+
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(1000);//毫秒
+                sum += 1;
+                System.out.println("thread1" + sum);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return sum;
+    }
+}
