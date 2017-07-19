5a845272a7e0472ce667eb8e0f45de594b19a672
cmshome <cmshome@163.com>
2017-07-19 10:23:24 +0800

    完善注释

--- src/com/lxk/reflect/CodeBlockTest.java
+++ src/com/lxk/reflect/CodeBlockTest.java
@@ -10,6 +10,17 @@ import com.lxk.reflect.classloaderAndForName.Line;
 public class CodeBlockTest {
     public static void main(String[] args) {
         System.out.println("主方法");
+        //普通代码块：在方法或语句中出现的{}就称为普通代码块。
+        //普通代码块和一般的语句执行顺序由他们在代码中出现的次序决定--“先出现先执行”
+        {
+            System.out.println("main方法中最开始的，普通代码块执行");
+        }
         Line line = new Line();
+        System.out.println("...............");
+        Line line1 = new Line();
+        System.out.println("...............");
+        {
+            System.out.println("main方法中结尾事的，普通代码块执行");
+        }
     }
 }
--- src/com/lxk/reflect/classloaderAndForName/Line.java
+++ src/com/lxk/reflect/classloaderAndForName/Line.java
@@ -4,6 +4,14 @@ package com.lxk.reflect.classloaderAndForName;
  * Created by lxk on 2017/2/21
  */
 public class Line {
+    //静态代码块:
+    //在java中使用static关键字声明的代码块。
+    //静态块用于初始化类，为类的属性初始化。每个静态代码块只会执行一次。
+    //由于JVM在加载类时会执行静态代码块，所以静态代码块先于主方法执行。
+    //如果类中包含多个静态代码块，那么将按照"先定义的代码先执行，后定义的代码后执行"。
+    //注意：
+    //1 静态代码块不能存在于任何方法体内。
+    //2 静态代码块不能直接访问静态实例变量和实例方法，需要通过类的实例对象来访问。
     static {
         System.out.println("静态代码块执行：loading line");
     }
@@ -19,11 +27,14 @@ public class Line {
         System.out.println("普通静态方法执行：loading line");
     }
 
-    {
-        System.out.println("要是普通的代码块呢？");
+    public Line() {
+        System.out.println("构造方法执行：loading line");
     }
 
-    public Line() {
-        System.out.println("构造方法执行");
+    //构造代码块：直接在类中定义且没有加static关键字的代码块称为{}构造代码块。
+    //构造代码块在创建对象时被调用，每次创建对象都会被调用，并且构造代码块的执行次序优先于类构造函数。
+    //这个构造代码块的执行顺序不会因为方法所在位置而影响，我特意将他放在构造函数之后。
+    {
+        System.out.println("构造代码块执行");
     }
 }
